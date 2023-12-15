65a940a98b56e54d4243493ea920a139f8d25a54
GUIGLI <joel.guigli@gmail.com>
2023-12-16 00:48:12 +0100

    ajout lien readme, tjrs mm pb valeur nan linreg

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ft_linear_regression
+
+https://towardsdatascience.com/linear-regression-using-gradient-descent-97a6c8700931
+
+-	parsing fichier data.csv avec check lignes manquantes
--- training_model.py
+++ training_model.py
@@ -13,26 +13,29 @@ def load(path: str) -> pd.DataFrame:
         print(f"Error handling: {str(e)}")
         return
 
+
 def estimate_price(theta0, theta1, mileage):
     return theta0 + theta1 * mileage
 
 
 def linear_regression(mileage: np.ndarray, price: np.ndarray):
+    
     m = len(mileage)
-    theta0, theta1 = 0.0, 0.0
-    learning_rate = 0.01
+    theta0, theta1 = 2, 1
+    learning_rate = 0.0001
 
-    for i in range(1000):
+    for i in range(100):
         predicted_price = estimate_price(theta0, theta1, mileage)
-        tmp_theta0 = learning_rate * (1 / m) * np.sum(predicted_price - price)
-        tmp_theta1 = learning_rate * (1 / m) * np.sum((predicted_price - price) * mileage)
+        # print(predicted_price)
+        d_theta0 = (1 / m) * np.sum(predicted_price - price)
+        d_theta1 = (1 / m) * np.sum((predicted_price - price) * mileage)
 
-        theta0 -= tmp_theta0
-        theta1 -= tmp_theta1
+        theta0 -= learning_rate * d_theta0
+        theta1 -= learning_rate * d_theta1
+        print(f"theta0 : {theta0}")
+        print(f"theta1 : {theta1}")
 
     return theta0, theta1
-
-
 
 
 def training_model():
@@ -40,25 +43,28 @@ def training_model():
     try:
         data = load("data.csv")
 
-        data_mileage = data['km'].values
-        data_price = data['price'].values
+        data_mileage = data['km'].values.astype('int')
+        data_price = data['price'].values.astype('int')
 
         mileage = np.array(data_mileage)
         price = np.array(data_price)
 
-        print(mileage)
-        print(price)
 
         theta0, theta1 = linear_regression(mileage, price)
+        predicted_price = estimate_price(theta0, theta1, mileage)
 
         print(f"theta0 : {theta0}")
         print(f"theta1 : {theta1}")
 
-        # plt.scatter(data_price, data_mileage)
-        # plt.title('Linear regression')
-        # plt.xlabel('Price')
-        # plt.ylabel('Mileage')
-        # plt.show()
+        print(data_mileage)
+        print(data_price)
+
+        plt.scatter(data_price, data_mileage)
+        plt.plot([min(price), max(price)], [min(predicted_price), max(predicted_price)], color='red')
+        plt.title('Linear regression')
+        plt.xlabel('Price')
+        plt.ylabel('Mileage')
+        plt.show()
     except Exception as e:
         print(f"Error handling: {str(e)}")
         return
