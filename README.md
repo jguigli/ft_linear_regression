ad6dfe26af8e0fda01de7bcec8c295dce2da5e57
GUIGLI <joel.guigli@gmail.com>
2023-12-17 23:36:55 +0100

    test algo reglin, prb de valeur trop grande datacsv

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # ft_linear_regression
 
-https://towardsdatascience.com/linear-regression-using-gradient-descent-97a6c8700931
 
 -	parsing fichier data.csv avec check lignes manquantes
+
+
+Descente de gradient :
+https://www.ibm.com/fr-fr/topics/gradient-descent
+
+https://towardsdatascience.com/linear-regression-using-gradient-descent-97a6c8700931
--- training_model.py
+++ training_model.py
@@ -21,19 +21,25 @@ def estimate_price(theta0, theta1, mileage):
 def linear_regression(mileage: np.ndarray, price: np.ndarray):
     
     m = len(mileage)
-    theta0, theta1 = 2, 1
-    learning_rate = 0.0001
+    theta0, theta1 = 0, 0
+    learning_rate = 0.01
 
-    for i in range(100):
+    for i in range(1000):
         predicted_price = estimate_price(theta0, theta1, mileage)
-        # print(predicted_price)
-        d_theta0 = (1 / m) * np.sum(predicted_price - price)
-        d_theta1 = (1 / m) * np.sum((predicted_price - price) * mileage)
+        # print(f"predicted price : {predicted_price}")
+        # print(f"price : {price}")
+        diff_price = predicted_price - price
+        # print(f"diff price : {diff_price}")
+        d_theta0 = (1 / m) * np.sum(diff_price)
+        d_theta1 = (1 / m) * np.sum((diff_price) * mileage)
+
+        # print(f"tmptheta0 : {d_theta0}")
+        # print(f"tmptheta1 : {d_theta1}")
 
         theta0 -= learning_rate * d_theta0
         theta1 -= learning_rate * d_theta1
-        print(f"theta0 : {theta0}")
-        print(f"theta1 : {theta1}")
+        # print(f"theta0 : {theta0}")
+        # print(f"theta1 : {theta1}")
 
     return theta0, theta1
 
@@ -43,27 +49,51 @@ def training_model():
     try:
         data = load("data.csv")
 
-        data_mileage = data['km'].values.astype('int')
-        data_price = data['price'].values.astype('int')
+        data_mileage = data['km'].astype('float')
+        data_price = data['price'].astype('float')
+        # data_mileage = 2 * np.random.rand(100, 1)
+        # data_price = 4 + 3 * data_mileage + np.random.randn(100, 1)
+
+        # data_mileage = np.array([[2400], [1398], [1505], [1855], [1760], [1148], [1668], [890], [1445], [840],
+        #               [820], [630], [740], [975], [670], [760], [482], [930], [609], [656],
+        #               [540], [685], [228], [617]])
+        # data_price = np.array([[36], [38], [44], [44], [52], [53], [58], [59], [59], [62],
+        #               [63], [63], [66], [68], [68], [69], [69], [69], [74], [75],
+        #               [79], [79], [79], [82]])
 
         mileage = np.array(data_mileage)
         price = np.array(data_price)
 
+        print(mileage)
+        print(price)
+
+        mileage_mean, mileage_std = np.mean(mileage), np.std(mileage)
+        price_mean, price_std = np.mean(price), np.std(price)
+
+        mileage = (mileage - mileage_mean) / mileage_std
+        price = (price - price_mean) / price_std
+
+        print(mileage)
+        print(price)
 
         theta0, theta1 = linear_regression(mileage, price)
         predicted_price = estimate_price(theta0, theta1, mileage)
 
-        print(f"theta0 : {theta0}")
-        print(f"theta1 : {theta1}")
+        # print(f"theta0 : {theta0}")
+        # print(f"theta1 : {theta1}")
+        print()
+        print(f"Y = {theta1}X + {theta0}")
+        # print(f"Predicted : {predicted_price}")
 
-        print(data_mileage)
-        print(data_price)
 
-        plt.scatter(data_price, data_mileage)
-        plt.plot([min(price), max(price)], [min(predicted_price), max(predicted_price)], color='red')
+        plt.scatter(data_mileage, data_price)
+        if theta1 < 0:
+            plt.plot([min(mileage), max(mileage)], [max(predicted_price), min(predicted_price)], color='red')
+        else:
+            plt.plot([min(mileage), max(mileage)], [min(predicted_price), max(predicted_price)], color='red')
         plt.title('Linear regression')
-        plt.xlabel('Price')
-        plt.ylabel('Mileage')
+        plt.xlabel('Mileage')
+        plt.ylabel('Price')
         plt.show()
     except Exception as e:
         print(f"Error handling: {str(e)}")
